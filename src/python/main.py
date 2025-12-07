d3ce4aed8a8e3de534300d8cfe550d1c5e9fb362
heloabrantessb <heloisa.abrantesdev@gmail.com>
2025-12-06 21:34:28 -0300

    feat: enhance main.py with database connection and initialization

--- src/python/main.py
+++ src/python/main.py
@@ -3,9 +3,16 @@ from dotenv import load_dotenv
 from pyswip import Prolog
 import mysql.connector
 
+load_dotenv()
+
 con = mysql.connector.connect(
     host = os.getenv("HOST"),
     user = os.getenv("USER"),
     password = os.getenv("PASSWORD"),
-    database = os.getenv("DATABASE")
+    database = os.getenv("DATABASE"),
 )
+
+print("Está funcionando!")
+
+prolog = Prolog()
+
